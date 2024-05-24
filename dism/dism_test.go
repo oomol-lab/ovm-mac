4d727ef3ff394d3488c570e8d1c51767baccce46
ihexon <14349453+ihexon@users.noreply.github.com>
2024-05-23 23:10:44 -0700

    feat(Dism): Add unit test for DismDisableFeature

--- dism/dism_test.go
+++ dism/dism_test.go
@@ -1,7 +1,38 @@
 package dism
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestDismDisableFeature(t *testing.T) {
+	testFeatureList := []struct {
+		name     string
+		expected error
+	}{
+		{"TFTP", nil},
+		{"TelnetClient", nil},
+	}
 
+	for _, test := range testFeatureList {
+		t.Run(test.name, func(t *testing.T) {
+
+			dismSession, err := OpenSession(DISM_ONLINE_IMAGE,
+				"",
+				"",
+				DismLogErrorsWarningsInfo,
+				"",
+				"")
+
+			if err != nil {
+				panic(err)
+			}
+			defer dismSession.Close()
+
+			result := dismSession.DisableFeature(test.name, "", false, nil, nil)
+			if !errors.Is(result, test.expected) {
+				t.Errorf("DismDisableFeature() expected %d, got %d", test.expected, result)
+			}
+		})
+	}
 }
